10ce9ce6887ada097ea02a8903197b5f8a23b17d
MarMassMedia <122909377+MarMassMedia@users.noreply.github.com>
2023-11-05 13:30:29 -0300

    Add files via upload

--- micodigosupersecreto.js
+++ micodigosupersecreto.js
@@ -1,15 +1,19 @@
+document.getElementById("suma").addEventListener("click", (e)=>{sumatoria(), false});
+document.getElementById("resta").addEventListener("click", (e)=>{restar(), false});
+document.getElementById("multiplicacion").addEventListener("click", (e)=>{multiplicar(), false});
+document.getElementById("division").addEventListener("click", (e)=>{dividir(), false});
+
 function sumatoria() {
-    document.getElementById("resultado").innerHTML = "su resultado es: " + (parseInt(document.getElementById("nro1").value) + parseInt(document.getElementById("nro2").value)) ; 
-}
+    document.getElementById("resultado").innerHTML = "su resultado es: " + (parseInt(document.getElementById("nro1").value) + parseInt(document.getElementById("nro2").value))};
 
 function restar() {
     document.getElementById("resultado").innerHTML = "su resultado es: " + (parseInt(document.getElementById("nro1").value) - parseInt(document.getElementById("nro2").value)) ; 
-}
+};
 
 function multiplicar() {
     document.getElementById("resultado").innerHTML = "su resultado es: " + (parseInt(document.getElementById("nro1").value) * parseInt(document.getElementById("nro2").value)) ; 
-}
+};
 
 function dividir() {
     document.getElementById("resultado").innerHTML = "su resultado es: " + (parseInt(document.getElementById("nro1").value) / parseInt(document.getElementById("nro2").value)) ; 
-}
+};
